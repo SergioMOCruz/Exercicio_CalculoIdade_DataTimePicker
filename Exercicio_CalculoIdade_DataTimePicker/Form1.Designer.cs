40223c7077d8804d354c614a7e8b024e8951f5c5
SergioMOCruz <75474370+SergioMOCruz@users.noreply.github.com>
2020-12-18 15:40:31 +0000

    Add project files.

---
+++ Exercicio_CalculoIdade_DataTimePicker/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace Exercicio_CalculoIdade_DataTimePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dTP = new System.Windows.Forms.DateTimePicker();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dTP
+            // 
+            this.dTP.Location = new System.Drawing.Point(61, 30);
+            this.dTP.Name = "dTP";
+            this.dTP.Size = new System.Drawing.Size(139, 20);
+            this.dTP.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(83, 56);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(93, 40);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 134);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.dTP);
+            this.Name = "Form1";
+            this.Text = "Cáculo Idade";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dTP;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Exercicio_CalculoIdade_DataTimePicker/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio_CalculoIdade_DataTimePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int idade = DateTime.Today.Year - dTP.Value.Year;
+
+            MessageBox.Show("Tem " + idade + " anos.");
+        }
+    }
+}
